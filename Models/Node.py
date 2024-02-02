6084bab687e3717f5b476c2e60f81cfc5c39d8b8
Daniel Belaish <daniel.bal0797@gmail.com>
2024-02-02 14:43:08 +0200

    fixed bug where two dimesnional boolean array was defined incorrectly

--- Models/Node.py
+++ Models/Node.py
@@ -1,15 +1,20 @@
+from z3 import Or
+
+
 class Node:
-    def __init__(self, node_id, is_initial, is_final):
+    def __init__(self, node_id, is_initial, is_final, n):
         self.properties = []
+        for rows in range(n):
+            self.properties.append([])
+            for columns in range(n):
+                self.properties[rows].append(False)
         self.node_id = node_id
         self.initial = is_initial
         self.final = is_final
 
-    def add_property(self, atomic_property):
-        if atomic_property not in self.properties:
-            self.properties.append(atomic_property)
-        else:
-            raise ValueError
+    def add_property(self, present, row, column):
+        if present:
+            self.properties[row][column] = True
 
     def __eq__(self, other):
         if isinstance(other, Node):
