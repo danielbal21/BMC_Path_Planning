69bb7d49b073a380eb1d5e1fd1e5b522811bfc02
Daniel Belaish <daniel.bal0797@gmail.com>
2024-02-12 22:13:21 +0200

    Modify Kripke to relate according to node ids

--- Models/Kripke.py
+++ Models/Kripke.py
@@ -2,25 +2,33 @@ class Kripke:
     def __init__(self):
         self.nodes = []
         self.relations = {}
+        self.count = 0
         pass
 
     def add_node(self, node):
         self.nodes.append(node)
+        self.count += 1
 
     def add_relation(self, node1, node2):
-        self.relations[node1.node_id] = node2.node_id
+        if not (node1.node_id in self.relations):
+            self.relations[node1.node_id] = set()
+        self.relations[node1.node_id].add(node2.node_id)
 
+    def add_relation_by_id(self, node1_id, node2_id):
+        if not (node1_id in self.relations):
+            self.relations[node1_id] = set()
+        self.relations[node1_id].add(node2_id)
     def are_related(self, node1, node2):
-        return node1 in self.nodes and node1 in self.relations and self.relations[node1.node_id] == node2.node_id
+        return node1 in self.nodes and node1 in self.relations and node2.node_id in self.relations[node1.node_id]
 
     def get_initial_state(self):
         for n in self.nodes:
-            if n.is_initial:
+            if n.initial:
                 return n
-        return False
+        return None
 
     def get_final_state(self):
         for n in self.nodes:
-            if n.is_final:
+            if n.final:
                 return n
-        return False
+        return None
